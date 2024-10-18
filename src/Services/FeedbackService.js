af532f7ab5ab0d52fa689aa0b80442910155cfd8
SamudraW <mattegoda15@gmail.com>
2024-10-18 20:09:45 +0530

    Update

---
+++ src/Services/FeedbackService.js
@@ -0,0 +1,49 @@
+import FeedbackRepository from '../Repositories/FeedbackRepository.js';
+
+const getAllFeedbacks = async () => {
+  return await FeedbackRepository.getAllFeedbacks();
+};
+
+const createFeedback = async (feedbackData) => {
+  return await FeedbackRepository.createFeedback(feedbackData);
+};
+
+const getFeedbackByEmail = async (email) => {
+  return await FeedbackRepository.getFeedbackByEmail(email);
+};
+
+const deleteFeedback = async (feedbackId) => {
+  return await FeedbackRepository.deleteFeedback(feedbackId);
+};
+
+const getFeedbackById = async (feedbackId) => {
+  return await FeedbackRepository.getFeedbackById(feedbackId);
+};
+
+const updateFeedback = async (feedbackId, feedbackData) => {
+  return await FeedbackRepository.updateFeedback(feedbackId, feedbackData);
+};
+
+const addResponse = async (feedbackId, response) => {
+  return await FeedbackRepository.addResponse(feedbackId, response);
+};
+
+const deleteResponse = async (feedbackId) => {
+  return await FeedbackRepository.deleteResponse(feedbackId);
+};
+
+const getFeedbacksByUserId = async (userId) => {
+  return await FeedbackRepository.getFeedbacksByUserId(userId);
+};
+
+export default {
+  getAllFeedbacks,
+  createFeedback,
+  getFeedbackByEmail,
+  deleteFeedback,
+  getFeedbackById,
+  updateFeedback,
+  addResponse,
+  deleteResponse,
+  getFeedbacksByUserId,
+};
