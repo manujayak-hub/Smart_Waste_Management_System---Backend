f5ef5476c4d266716dc7c9bb227a3acd048dd59a
Anjana Gunarathna <anjanagunarathna01@gmail.com>
2024-10-18 21:17:13 +0530

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import "dotenv/config"
 
 import UserRoute from './src/Routes/UserRoute.js'
 
-import Schedule_Router from "./src/Routes/ScheduleRoute.js"
+import ScheduleRouter from "./src/Routes/ScheduleRoute.js"
 import PamentRoute from './src/Routes/PaymentRoute.js'
 import userpay from './src/Routes/UserPayRoute.js'
 
@@ -36,7 +36,7 @@ next()
 //routes
 app.use('/auth',UserRoute)
 
-app.use('/schedule',ScheduleRouter)
+app.use('/schedule',ScheduleRouter);
 
 app.use('/api/payments', PamentRoute);
 app.use('/api/userpay', userpay);
